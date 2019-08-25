63b4e2ecac9c9b1f7ef2084e2dbc018d8842c2ec
Ylannl <r.y.peters@tudelft.nl>
2019-08-25 19:25:51 +0200

    use cmake --build in all workflow files

--- cmake/apple-llvm7.cmake
+++
@@ -1,7 +0,0 @@
-set(CMAKE_C_COMPILER "/usr/local/opt/llvm/bin/clang")
-set(CMAKE_CXX_COMPILER "/usr/local/opt/llvm/bin/clang++")
-set(CMAKE_EXE_LINKER_FLAGS -Wl)
-set(CMAKE_MACOSX_RPATH 1)
-set(CMAKE_INSTALL_RPATH "/usr/local/opt/llvm/lib")
-set(CMAKE_EXE_LINKER_FLAGS "-L/usr/local/opt/llvm/lib")
-# link_directories("/usr/local/opt/llvm/lib")
