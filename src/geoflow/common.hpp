217204c066c5ddd21209fba59d3e209b5374fff5
Ylannl <r.y.peters@tudelft.nl>
2019-12-18 15:22:20 +0100

    dont need filesystem in common.hpp

--- src/geoflow/common.hpp
+++ src/geoflow/common.hpp
@@ -24,18 +24,6 @@
 #include <exception>
 #include <algorithm>
 
-#if defined(__cplusplus) && __cplusplus >= 201703L && defined(__has_include)
-  #if __has_include(<filesystem>)
-    #define GHC_USE_STD_FS
-    #include <filesystem>
-    namespace fs = std::filesystem;
-  #endif
-#endif
-#ifndef GHC_USE_STD_FS
-  #include <ghc/filesystem.hpp>
-  namespace fs = ghc::filesystem;
-#endif
-
 namespace geoflow {
 
 class gfException: public std::exception
