051f32def9c96b88d891dcea8142282201e68d21
Ylannl <r.y.peters@tudelft.nl>
2020-03-31 21:24:34 +0200

    print dll loading error message on windows

--- src/DLLoader/Windows/DLLoader.h
+++ src/DLLoader/Windows/DLLoader.h
@@ -29,10 +29,32 @@ namespace dlloader
 
 		~DLLoader() = default;
 
+    
+    //Returns the last Win32 error, in string format. Returns an empty string if there is no error.
+    std::string GetLastErrorAsString()
+    {
+        //Get the error message, if any.
+        DWORD errorMessageID = ::GetLastError();
+        if(errorMessageID == 0)
+            return std::string(); //No error message has been recorded
+
+        LPSTR messageBuffer = nullptr;
+        size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                                    NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+
+        std::string message(messageBuffer, size);
+
+        //Free the buffer.
+        LocalFree(messageBuffer);
+
+        return message;
+    }
+
 		bool DLOpenLib() override
 		{
 			if (!(_handle = LoadLibrary(_pathToLib.c_str()))) {
 				std::cerr << "Can't open and load " << _pathToLib << std::endl;
+				std::cerr << "ERROR: " << GetLastErrorAsString() << std::endl;
 				return false;
 			}
 
