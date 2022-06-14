ea73ddac452fa0d190f6e57e7b19bca2163eee16
Ravi Peters <r.y.peters@tudelft.nl>
2022-06-14 11:53:06 +0200

    implement CLI with argh!

--- apps/geoflow-app.cpp
+++ apps/geoflow-app.cpp
@@ -42,7 +42,7 @@
   #include <geoflow/core_nodes.hpp>
 #endif
 
-#include "CLI11.hpp"
+#include "argh.h"
 
 using namespace geoflow;
 
@@ -120,6 +120,38 @@ void print_nodes(NodeRegisterMap& node_registers) {
   }
 }
 
+void print_usage(std::string program_name) {
+  std::cout << "Usage:\n\n";
+  std::cout << program_name << " [--version | --list-nodes | --list-plugins] [<flowchart file> [-g] | [-w] [-c <config file>] [--PARAM1=value1 --PARAM2=value2 ...] ]";
+//     cli.add_flag("--verbose", verbose, "Print verbose messages");
+//     auto info_options = cli.add_option_group("Info", "Debug information");
+
+//     auto version_flag = info_options->add_flag("--version,-v", "Print version information");
+//     auto plugins_flag = info_options->add_flag("--plugins,-p", "List available plugins");
+//     auto nodes_flag = info_options->add_flag("--nodes,-n", "List available nodes from plugins that are loaded");
+//     auto run_subcommand = cli.add_subcommand("run", "Load and run flowchart");
+//     auto opt_flowchart_path = run_subcommand->add_option("flowchart", flowchart_path, "Flowchart file");
+//     auto opt_list_globals = run_subcommand->add_flag("--globals,-g", "List flowchart globals. Skips flowchart execution");
+//     auto opt_workdir = run_subcommand->add_flag("--workdir,-w", "Set working directory to folder containing flowchart file");
+   
+//     auto config_subcommand = cli.add_subcommand("set", "Set flowchart globals (comes after run). Set the parameters as '--parameter1=value1 --parameter2=value2 ...'.");
+
+//        auto config_file = config_subcommand->set_config("--config,-c", "", "Read globals from config file");
+
+//     // CLI::Option* opt_plugin_folder = cli.add_option("-p,--plugin-folder", plugin_folder, "Plugin folder");
+//     // if(*opt_plugin_folder) {
+//     //   std::cout << "Setting plugin folder to " << plugin_folder << "\n";
+//     // }
+
+
+//     // CLI::Option* opt_log = cli.add_option("-l,--log", log_filename, "Write log to file");
+//     // opt_log->check([](const std::string& s)->std::string {
+//     //   if(!fs::exists(fs::absolute(fs::path(s)).parent_path())) {
+//     //     return std::string("Path to log file does not exist");
+//     //   } else return std::string();
+//     // });
+}
+
 int main(int argc, const char * argv[]) {
   std::string flowchart_path = "flowchart.json";
   std::string plugin_folder = GF_PLUGIN_FOLDER;
@@ -132,151 +164,132 @@ int main(int argc, const char * argv[]) {
     std::cout << "Detected environment variable GF_PLUGIN_FOLDER = " << plugin_folder << "\n";
   }
 
+  auto cmdl = argh::parser(argc, argv);
+  std::string program_name = cmdl[0];
+
+  if(cmdl[{ "-v", "--version" }]) {
+    print_version();
+    return EXIT_SUCCESS;
+  }
+
+  if(cmdl[{ "-h", "--help" }]) {
+    print_usage(program_name);
+    return EXIT_SUCCESS;
+  }
+
   PluginManager plugin_manager;
   bool verbose{false};
-
   {
     NodeRegisterMap node_registers;
     NodeManager flowchart(node_registers);
+    
+    verbose = cmdl[{ "--verbose" }];
     load_plugins(plugin_manager, node_registers, plugin_folder, verbose);
 
-    CLI::App cli{"Geoflow"};
-
-    // CLI::Option* opt_plugin_folder = cli.add_option("-p,--plugin-folder", plugin_folder, "Plugin folder");
-    // if(*opt_plugin_folder) {
-    //   std::cout << "Setting plugin folder to " << plugin_folder << "\n";
-    // }
-    cli.add_flag("--verbose", verbose, "Print verbose messages");
-    auto info_options = cli.add_option_group("Info", "Debug information");
-
-    auto version_flag = info_options->add_flag("--version,-v", "Print version information");
-    auto plugins_flag = info_options->add_flag("--plugins,-p", "List available plugins");
-    auto nodes_flag = info_options->add_flag("--nodes,-n", "List available nodes from plugins that are loaded");
-
-    // CLI::Option* opt_log = cli.add_option("-l,--log", log_filename, "Write log to file");
-    // opt_log->check([](const std::string& s)->std::string {
-    //   if(!fs::exists(fs::absolute(fs::path(s)).parent_path())) {
-    //     return std::string("Path to log file does not exist");
-    //   } else return std::string();
-    // });
-
-    auto run_subcommand = cli.add_subcommand("run", "Load and run flowchart");
-    auto opt_flowchart_path = run_subcommand->add_option("flowchart", flowchart_path, "Flowchart file");
-    auto opt_list_globals = run_subcommand->add_flag("--globals,-g", "List flowchart globals. Skips flowchart execution");
-    auto opt_workdir = run_subcommand->add_flag("--workdir,-w", "Set working directory to folder containing flowchart file");
-    opt_flowchart_path->check(CLI::ExistingFile);
-
-    opt_flowchart_path->required();
-    run_subcommand->fallthrough();
-
-    auto config_subcommand = cli.add_subcommand("set", "Set flowchart globals (comes after run). Set the parameters as '--parameter1=value1 --parameter2=value2 ...'.");
-    config_subcommand->needs(run_subcommand);
-    auto config_file = config_subcommand->set_config("--config,-c", "", "Read globals from config file");
-    config_subcommand->allow_config_extras(); // for globals
-
-    // CLI11 Callbacks
-    info_options->parse_complete_callback([&]() -> void {
-      // std::cout << "info_options->parse_complete_callback\n";
-
-      if(*version_flag) {
-        print_version();
-      }
-
-      if(*plugins_flag) {
-        print_plugins(node_registers);
-      }
-
-      if(*nodes_flag) {
-        print_nodes(node_registers);
-      }
-    });
-    std::map<std::string, std::vector<std::string>> globals_from_cli;
-    run_subcommand->parse_complete_callback([&]() -> void {
-      // std::cout << "run_subcommand->parse_complete_callback\n";
-      // load flowchart from file
-      if(*opt_flowchart_path) {
-        // set current work directory to folder containing flowchart file
-        auto abs_path = fs::absolute(fs::path(flowchart_path));
-        flowchart_folder = abs_path.parent_path();
-        flowchart_path = abs_path.string();
-        // fs::current_path(flowchart_folder);
-        flowchart.load_json(flowchart_path);
-        // fs::current_path(launch_path);
-      }
-
-      // handle globals overrides provided by user
-      if (*opt_list_globals) std::cout << "Available globals:\n";
-      for (auto&[key,val] : flowchart.global_flowchart_params) {
-        auto [it, inserted] = globals_from_cli.emplace(std::make_pair(key, vec1s{}));
-        config_subcommand->add_option("--"+key, (it->second), "");
-        if (*opt_list_globals) {
-          std::cout << " > " << key;
-          if (!val->get_help().empty()) std::cout << " [" << val->get_help() << "]";
-          std::cout << "\n   default value: " << val->as_json() << "\n";
-        }
-      }
-      // sc_globals.parse(run_subcommand->remaining_for_passthrough());
-    });
-    config_subcommand->preparse_callback([&](size_t n) -> void {
-      // std::cout << "config_subcommand->preparse_callback\n";
-    });
-    config_subcommand->parse_complete_callback([&]() -> void {
-      // std::cout << "config_subcommand->parse_complete_callback\n";
-      // process global values from cli/config file
-      if (*config_file) std::cout << "Reading configuration from file " << config_subcommand->get_config_ptr()->as<std::string>() << std::endl;
-      if(config_subcommand->count()) {
-        for (auto& [key, values] : globals_from_cli) {
-          if (values.size()) {
-            std::string concat_values{};
-            for (auto& value : values) {
-              concat_values += value + " ";
-            }
-            concat_values.pop_back();
-            std::cout << "set global " << key << " = " << concat_values << "\n";
-            
-            auto& g = flowchart.global_flowchart_params[key];
-            try{
-              if (g->is_type(typeid(std::string))) {
-                auto* gptr = static_cast<ParameterByValue<std::string>*>(g.get());
-                gptr->set(concat_values);
-              } else if (g->is_type(typeid(float))) {
-                auto* gptr = static_cast<ParameterByValue<float>*>(g.get());
-                gptr->set(std::stof(concat_values));
-              } else if(g->is_type(typeid(int))) {
-                auto* gptr = static_cast<ParameterByValue<int>*>(g.get());
-                gptr->set(std::stoi(concat_values));
-              } else if(g->is_type(typeid(bool))) {
-                auto* gptr = static_cast<ParameterByValue<bool>*>(g.get());
-                if(concat_values == "true")
-                  gptr->set(true);
-                else if(concat_values == "false")
-                  gptr->set(false);
-                else throw gfFlowchartError("failed to get boolean from string");
-              }
-            } catch (const std::exception& e) {
-              std::cerr << "Error in parsing global parameters\n";
-              std::cerr << e.what();
-            }
-          }
-        }
-      }
-    });
-
-    try {
-      cli.parse(argc, argv);
-    } catch (const CLI::ParseError &e) {
-      return cli.exit(e);
+    if(cmdl[{ "-p", "--list-plugins" }]) {
+      print_plugins(node_registers);
+      return EXIT_SUCCESS;
+    } else if(cmdl[{ "-n", "--list-nodes" }]) {
+      print_nodes(node_registers);
+      return EXIT_SUCCESS;
     }
-    
+
+    // check for flowchart
+    std::map<std::string, std::vector<std::string>> globals_from_cli;
+    if(cmdl[1].empty()) {
+      std::cerr << "Error, no flowchart path provided!\n";
+      print_usage(program_name);
+      return EXIT_FAILURE;
+    }
+
+    flowchart_path = cmdl[1];
+    if (!fs::exists(flowchart_path)) {
+      std::cerr << "Error, no such flowchart file: " << flowchart_path << "\n";
+      print_usage(program_name);
+      return EXIT_FAILURE;
+    }
+
+    // set current work directory to folder containing flowchart file
+    auto abs_path = fs::absolute(fs::path(flowchart_path));
+    flowchart_folder = abs_path.parent_path();
+    flowchart_path = abs_path.string();
+    // fs::current_path(flowchart_folder);
+    flowchart.load_json(flowchart_path);
+    // fs::current_path(launch_path);
+
+    // handle globals overrides provided by user
+    bool list_globals = cmdl[{"-g", "--list-globals"}];
+    if (list_globals) std::cout << "Available globals:\n";
+    for (auto&[key,val] : flowchart.global_flowchart_params) {
+      auto [it, inserted] = globals_from_cli.emplace(std::make_pair(key, vec1s{}));
+      // config_subcommand->add_option("--"+key, (it->second), "");
+      if (list_globals) {
+        std::cout << " > " << key;
+        if (!val->get_help().empty()) std::cout << " [" << val->get_help() << "]";
+        std::cout << "\n   default value: " << val->as_json() << "\n";
+      }
+    }
+    if (list_globals) return EXIT_SUCCESS;  
+
+    // process global values from cli/config file
+    std::string config_path;
+    if (cmdl[{"-c", "--config"}]) {
+      if (!(cmdl({"-c", "--config"}) >> config_path)) {
+        std::cerr << "Error, no config file provided\n";
+        print_usage(program_name);
+        return EXIT_FAILURE;
+      } else {
+        if (!fs::exists(config_path)) {
+          std::cerr << "Error, no such config file: " << config_path << "\n";
+          print_usage(program_name);
+          return EXIT_FAILURE;
+        }
+        std::cout << "Reading configuration from file " << config_path << std::endl;
+      }
+    }
+    for (auto& [key, value] : cmdl.params()) {
+      std::cout << "set global " << key << " = " << value << "\n";
+      
+      auto& g = flowchart.global_flowchart_params[key];
+      try{
+        if (g->is_type(typeid(std::string))) {
+          std::string s;
+          if (!(cmdl(key) >> s)) throw gfFlowchartError("Unable to set string from provided value (" + value + ")");
+          auto* gptr = static_cast<ParameterByValue<std::string>*>(g.get());
+          gptr->set(s);
+        } else if (g->is_type(typeid(float))) {
+          float f;
+          if (!(cmdl(key) >> f)) throw gfFlowchartError("Unable to set float from provided value (" + value + ")");
+          auto* gptr = static_cast<ParameterByValue<float>*>(g.get());
+          gptr->set(f);
+        } else if(g->is_type(typeid(int))) {
+          int i;
+          if (!(cmdl(key) >> i)) throw gfFlowchartError("Unable to set integer from provided value (" + value + ")");
+          auto* gptr = static_cast<ParameterByValue<int>*>(g.get());
+          gptr->set(i);
+        } else if(g->is_type(typeid(bool))) {
+          auto* gptr = static_cast<ParameterByValue<bool>*>(g.get());
+          if(value == "true")
+            gptr->set(true);
+          else if(value == "false")
+            gptr->set(false);
+          else throw gfFlowchartError("Unable set boolean global from provided value (" + value + "). Please use 'true' or 'false'.");
+        }
+      } catch (const std::exception& e) {
+        std::cerr << "Error in parsing global parameter '" << key << "':\n";
+        std::cerr << e.what() << std::endl;
+      }
+    }
+
     #ifndef GF_BUILD_WITH_GUI
-      bool no_arguments = !(*run_subcommand) && !(*version_flag) && !(*nodes_flag) && !(*plugins_flag) && !(verbose);
+      bool no_arguments = false;//!(*run_subcommand) && !(*version_flag) && !(*nodes_flag) && !(*plugins_flag) && !(verbose);
       if(no_arguments) {
-        std::cout << cli.help() << std::flush;
+        // std::cout << cli.help() << std::flush;
         return 1;
       }
     #endif
 
-    if( ! *opt_list_globals ) {
+    if( ! list_globals ) {
       if( ! verbose ) {
         std::clog.setstate(std::ios_base::failbit);
         std::cout.setstate(std::ios_base::failbit);
@@ -284,7 +297,7 @@ int main(int argc, const char * argv[]) {
       }
 
       // launch gui or just run the flowchart in cli mode
-      if(*opt_workdir) fs::current_path(flowchart_folder);
+      if(cmdl[{"-w", "--workdir"}]) fs::current_path(flowchart_folder);
       #ifdef GF_BUILD_WITH_GUI
         try {
         launch_gui(flowchart, flowchart_path);
@@ -296,7 +309,7 @@ int main(int argc, const char * argv[]) {
         }
       #else
         try {
-          if (*run_subcommand) flowchart.run_all();
+          flowchart.run_all();
         }
         catch (const gfException& e) {
           // std::cerr.clear();
