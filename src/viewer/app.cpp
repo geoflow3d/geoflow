7f8e6d4603811fd2c4b2f036d057ed9007650e4c
Ravi Peters <r.y.peters@tudelft.nl>
2022-06-17 14:36:36 +0200

    update copyright notices

--- src/viewer/app.cpp
+++ src/viewer/app.cpp
@@ -1,5 +1,5 @@
 // This file is part of Geoflow
-// Copyright (C) 2018-2019  Ravi Peters, 3D geoinformation TU Delft
+// Copyright (C) 2018-2022 Ravi Peters
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
